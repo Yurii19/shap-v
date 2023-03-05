a698f58910ce611768abb0135a1fad0b655e0305
Yurii19 <yurikolechkin@gmail.com>
2023-03-05 17:08:29 +0200

    added: vue3-google-signin npm package

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import Home from './views/Home.vue';
 import Form from './views/Form.vue';
+import GoogleSignInPlugin from 'vue3-google-signin';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,6 +14,12 @@ const router = createRouter({
 });
 
 const app = createApp(App);
+
+app.use(GoogleSignInPlugin, {
+  clientId:
+    '776632151768-dt2qeel2mfdadh6caugakqu0kdqfcn65.apps.googleusercontent.com',
+});
+//CLIENT ID OBTAINED FROM GOOGLE API CONSOLE
 
 app.use(router);
 
